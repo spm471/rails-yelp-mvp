c6d85e31320a6ca80900274c396e22004389749d
SebastianoPio Matera <spm471@nyu.edu>
2019-08-07 16:55:05 +0200

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+Restaurant.destroy_all
+
+10.times do
+  restaurant = Restaurant.new(
+    name: Faker::Book.title,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: %w[chinese japanese italian french belgian].sample
+  )
+  restaurant.save!
+end
